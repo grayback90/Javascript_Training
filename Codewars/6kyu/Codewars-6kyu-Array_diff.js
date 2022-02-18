ea2ce9d90aaf083a37df3c759301d3797c23f6e8
RituGray <kenny-maeder@web.de>
2022-02-18 14:04:35 +0100

    Solution for Array_diff

--- Codewars/6kyu/Codewars-6kyu-Array_diff.js
+++ Codewars/6kyu/Codewars-6kyu-Array_diff.js
@@ -1,12 +1,28 @@
 // ToDo
-// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+// Your goal in this Kata is to implement a difference function, which subtracts one list from another and returns the result.
 // It should remove all values from list a, which are present in list b keeping their order.
 // example: arrayDiff([1,2],[1]) == [2]
 // If a value is present in b, all of its occurrences must be removed from the other:
 // example: arrayDiff([1,2,2,2,3],[2]) == [1,3]
 
 function arrayDiff(a, b) {
-  
+    let returnArr = [];
+    
+    if(b.length === 0){
+      return a;
+    }
+    
+    if(a.length === 0) {
+      return [];
+    }
+    
+    a.forEach(function included(element){
+      if(!b.includes(element)){
+        returnArr.push(element);
+      }    
+    })
+    
+    return returnArr;
 }
 
 // Test
@@ -23,5 +39,14 @@ function arrayDiff(a, b) {
 
 // Other Solutions
 /*
+    function array_diff(a, b) {
+        return a.filter(e => !b.includes(e));
+    }
+*/
 
+// Other Solutions v2
+/*
+    function array_diff(a, b) {
+        return a.filter(function(x) { return b.indexOf(x) == -1; });
+    }
 */
