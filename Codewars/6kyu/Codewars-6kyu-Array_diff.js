4c1bf68df60d8c27a55bb96280562e1c8352a73d
RituGray <kenny-maeder@web.de>
2022-02-18 11:29:21 +0100

    Create Codewars-6kyu-Array_diff.js

---
+++ Codewars/6kyu/Codewars-6kyu-Array_diff.js
@@ -0,0 +1,27 @@
+// ToDo
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+// It should remove all values from list a, which are present in list b keeping their order.
+// example: arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+// example: arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+function arrayDiff(a, b) {
+  
+}
+
+// Test
+/*
+    describe("Sample tests", function() {
+        it("Should pass Sample tests", function() {
+            Test.assertDeepEquals(arrayDiff([], [4,5]), [], "a was [], b was [4,5]");
+            Test.assertDeepEquals(arrayDiff([3,4], [3]), [4], "a was [3,4], b was [3]");
+            Test.assertDeepEquals(arrayDiff([1,8,2], []), [1,8,2], "a was [1,8,2], b was []");
+            Test.assertDeepEquals(arrayDiff([1,2,3], [1,2]), [3], "a was [1,2,3], b was [1,2]")
+        });
+    }); 
+*/
+
+// Other Solutions
+/*
+
+*/
