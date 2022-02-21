4c449df4df1b82e2972fbfa28a859b56bc304421
RituGray <kenny-maeder@web.de>
2022-02-21 10:26:16 +0100

    Create Codewars-5kyu-Calculating_with_functions.js

---
+++ Codewars/5kyu/Codewars-5kyu-Calculating_with_functions.js
@@ -0,0 +1,47 @@
+// ToDo
+// This time we want to write calculations using functions and get the results. Let's have a look at some examples:
+// seven(times(five())); // must return 35
+// four(plus(nine())); // must return 13
+// eight(minus(three())); // must return 5
+// six(dividedBy(two())); // must return 3
+//
+// Rules
+// There must be a function for each number from 0 ("zero") to 9 ("nine")
+// There must be a function for each of the following mathematical operations: plus, minus, times, dividedBy
+// Each calculation consist of exactly one operation and two numbers
+// The most outer function represents the left operand, the most inner function represents the right operand
+// Division should be integer division. For example, return 2, not 2.666666...
+
+function zero() {}
+function one() {}
+function two() {}
+function three() {}
+function four() {}
+function five() {}
+function six() {}
+function seven() {}
+function eight() {}
+function nine() {}
+
+function plus() {}
+function minus() {}
+function times() {}
+function dividedBy() {}
+
+// Test
+/*
+    describe("Tests", () => {
+        it("test", () => {
+            Test.assertEquals(seven(times(five())), 35);
+            Test.assertEquals(four(plus(nine())), 13);
+            Test.assertEquals(eight(minus(three())), 5);
+            Test.assertEquals(six(dividedBy(two())), 3);
+        });
+    });
+
+*/
+
+// Other Solutions
+/*
+
+*/
